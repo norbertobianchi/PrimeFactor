95f2851db7277c8e564dfc4264cd76291329675a
Stefano Leli <stefano.leli@agilereloaded.it>
2016-07-07 10:40:58 +0200

    First Commit

---
+++ test/primefactor/PrimeFactorTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primefactor;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author stefanoleli
+ */
+public class PrimeFactorTest {
+    
+    public PrimeFactorTest() {
+    }
+
+    @Test
+    public void testMain() {
+       
+    }
+    
+}
